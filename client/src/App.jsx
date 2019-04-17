b61c0d9215a3a8712e23292bb069a845e6d7151a
nerymartinezgonzalez <nery.martinez.gonzalez@gmail.com>
2019-04-17 14:45:35 -0500

    rendering map component/debug dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 // RHL only for front end development
 import { hot } from 'react-hot-loader';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { GOOGLE_API } from '../../.env'
 
-class App extends Component {
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
   render() {
-    return <div>Welcome to a Modern Minimal React Boilerplate</div>;
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: 19.4220866,
+         lng: -99.1718937
+        }}
+      />
+    );
   }
 }
-// hot export works with RHL. Remove line 11 when starting fullstack integration
-export default hot(module)(App);
-// Uncomment line 13 & delete line 11 when starting fullstack integration
-// export default App;
+
+export default GoogleApiWrapper({
+  apiKey:  GOOGLE_API
+})(MapContainer);
+
+// class App extends Component {
+//   render() {
+//     return <div>Welcome to a Modern Minimal React Boilerplate</div>;
+//   }
+// }
+//  hot export works with RHL. Remove line 11 when starting fullstack integration
+// export default hot(module)(App);
+//  Uncomment line 13 & delete line 11 when starting fullstack integration
+//  export default App;
