a17c4a317f883a015fa4112ed93dcd3142df35a9
nerymartinezgonzalez <nery.martinez.gonzalez@gmail.com>
2019-04-20 12:27:29 -0500

    working on get request

--- server/config.js
+++ server/config.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const compression = require('compression');
+const database = require('../database/mysql.js');
 
 const app = express();
+const path = require('path');
 
-//middleware
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/../client/dist"));
+
+
+app.get('/address', function(req, res){
+  database.selectAll((err, data) => {
+    if(err){
+      console.log('error connecting to the database');
+      res.sendStatus(500);
+    }else {
+      res.status(200).json(data);
+    }
+  })
+})
+//middleware
 
 //HTTP Requests go here
 
