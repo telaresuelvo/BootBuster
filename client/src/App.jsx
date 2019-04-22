140489a591b2f162e08894b7f84ba0799f0c9f1c
ClaudioGage <gageclaudio@gmail.com>
2019-04-22 12:46:27 -0500

    render the InitialView-Home element onward to separation and integration of components to the other two mayor views

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,62 +1,26 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 import { GOOGLE_API } from "../../.env";
-import MapContainer from "./MapContainer.jsx";
-import CurrentLocation from './Map.jsx';
-import Button from './SaveButton.jsx';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import InitialView from "./InitialView";
 
- class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       items:[]
+      items: []
     };
-    //this.addItem = this.addItem.bind(this);
-    this.getData = this.getData.bind(this);
-    //this.postData = this.postData.bind(this);
-    //this.deleteItem = this.deleteItem.bind(this);
   }
-  componentDidMount(){
-   this.getData('/address')
- }
-
- getData() {
-   return fetch('/address')
-   .then(response => response.JSON())
-   .then(data => {
-     this.setState({
-       items: data,
-    });
-  })
-  .catch(err => console.error(err));
-}
-//postData('', data = {}) {
-//   return fetch({
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },s
-//     body: JSON.strigify(data),
-//   })
-//   then(() => this.getData())
-//   .catch(err => console.error(err));
-// }
-//
-// addItem(){
-//   let {} = this.state;
-//   const newItem = {
-//     id:list.length + 1,
-//   };
 
   render() {
-    console.log(this.state.items);
-      return(
-    <div>
-      <MapContainer />
-      <Button />
-      <CurrentLocation />
-    </div>
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/" component={InitialView} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
-export default App
---
+++ client/src/InitialView.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import { GOOGLE_API } from "../../.env";
+import MapContainer from "./MapContainer.jsx";
+import CurrentLocation from "./Map.jsx";
+
+class InitialView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+    //this.addItem = this.addItem.bind(this);
+    this.getData = this.getData.bind(this);
+    //this.postData = this.postData.bind(this);
+    //this.deleteItem = this.deleteItem.bind(this);
+  }
+  componentDidMount() {
+    this.getData("/address");
+  }
+
+  getData() {
+    return fetch("/address")
+      .then(response => response.JSON())
+      .then(data => {
+        this.setState({
+          items: data
+        });
+      })
+      .catch(err => console.error(err));
+  }
+  //postData('', data = {}) {
+  //   return fetch({
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },s
+  //     body: JSON.strigify(data),
+  //   })
+  //   then(() => this.getData())
+  //   .catch(err => console.error(err));
+  // }
+  //
+  // addItem(){
+  //   let {} = this.state;
+  //   const newItem = {
+  //     id:list.length + 1,
+  //   };
+
+  render() {
+    console.log(this.state.items);
+    return (
+      <div>
+        <MapContainer />
+      </div>
+    );
+  }
+}
+export default InitialView;
