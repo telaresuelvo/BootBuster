e7bd19da3256d9edfa126af8783a84018a411968
ClaudioGage <gageclaudio@gmail.com>
2019-04-26 20:05:37 -0500

    managed to create an alarm/ bugg with the alert pop up

---
+++ client/src/Alarms.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+
+export default class AlarmClock extends Component {
+  constructor() {
+    super();
+    this.state = {
+      alarmTime: ""
+    };
+    this.setAlarmTime = this.setAlarmTime.bind(this);
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.checkAlarmClock(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  setAlarmTime(event) {
+    event.preventDefault();
+    const inputAlarmTimeModified = "02";
+    this.setState({
+      alarmTime: inputAlarmTimeModified
+    });
+  }
+
+  checkAlarmClock() {
+    if (this.state.alarmTime == "undefined" || !this.state.alarmTime) {
+      this.alarmMessage = "Please set your alarm.";
+    } else {
+      this.alarmMessage = "Your alarm is set for " + this.state.alarmTime + ".";
+      if (this.props.toc === this.state.alarmTime) {
+        setTimeout(function() {
+          alert("Hello");
+        }, 3000);
+      } else {
+        console.log("not yet");
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>React Alarm Clock</h1>
+        <h2>It is {this.props.toc}.</h2>
+        <h2>{this.alarmMessage}</h2>
+        <form>
+          <button onClick={this.setAlarmTime}>5 min</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+/*
+  constructor(props) {
+    super(props);
+    this.state = {
+      alarmTime: "undefined"
+    };
+    this.setAlarmTime = this.setAlarmTime.bind(this);
+    this.checkAlarmClock = this.checkAlarmClock.bind(this);
+    this.combo = this.combo.bind(this);
+  }
+
+  setAlarmTime() {
+    const inputAlarmTimeModified = "02";
+    this.setState({
+      alarmTime: inputAlarmTimeModified
+    });
+  }
+
+  checkAlarmClock() {
+    if (this.props.toc === this.state.alarmTime) {
+      alert("its time!");
+    } else {
+      console.log("not yet");
+    }
+  }
+  combo(event) {
+    this.setAlarmTime();
+    this.checkAlarmClock();
+  }
+
+  render() {
+    console.log("props from Two", this.props.toc);
+    console.log("2 passing props?", this.state.alarmTime);
+    return (
+      <div>
+        <h1>React Alarm Clock</h1>
+        <h2>It is {this.props.toc}.</h2>
+        <button onClick={this.combo}>9 min</button>
+      </div>
+    );
+  }
+}
+*/
